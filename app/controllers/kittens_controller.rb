d5884c1b692ff5c29dde9f6e16aa5f18560fa04c
James Helton <trealtrix@gmail.com>
2016-06-16 16:36:59 -0400

    Add Kitten MVC

---
+++ app/controllers/kittens_controller.rb
@@ -0,0 +1,38 @@
+class KittensController < ApplicationController
+include KittensHelper
+  def index
+    @kittens = Kitten.all
+  end
+
+  def show
+    @kitten = Kitten.find(params[:id])
+  end
+
+  def new
+    @kitten = Kitten.new
+  end
+
+  def create
+    Kitten.create(kitten_params)
+    flash.notice = "Kitten saved"
+    redirect_to root_path
+  end
+
+  def edit
+    @kitten = Kitten.find(params[:id])
+  end
+
+  def update
+    @kitten = Kitten.find(params[:id])
+    @kitten.update(kitten_params)
+    flash.notice = "Kitten updated"
+    redirect_to kitten_path(@kitten)
+  end
+
+  def destroy
+    @kitten = Kitten.find(params[:id])
+    @kitten.destroy
+    flash.notice = "Kitten '#{@kitten.name}' has been removed"
+    redirect_to kittens_path
+  end
+end
